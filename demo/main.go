453bf21e5eaca98c7818343a6b02f29f4b60b2d8
wnz27 <wnz27@icloud.com>
2024-03-16 22:54:47 +0800

    feat: init sign func

--- demo/main.go
+++ demo/main.go
@@ -2,15 +2,15 @@
  * @Author: 27
  * @LastEditors: 27
  * @Date: 2024-03-16 18:33:55
- * @LastEditTime: 2024-03-16 22:23:58
- * @FilePath: /k-infra/douyin/main.go
+ * @LastEditTime: 2024-03-16 22:35:49
+ * @FilePath: /k-infra/demo/main.go
  * @description: type some description
  */
-package douyin
+package demo
 
 import "fmt"
 
 // test
 func DoyinTestDemo() {
-	fmt.Println("This is douyin test demo")
+	fmt.Println("This is test demo!")
 }
---
+++ doyin_sdk/doc.go
@@ -0,0 +1,12 @@
+/*
+ * @Author: 27
+ * @LastEditors: 27
+ * @Date: 2024-03-16 22:52:09
+ * @LastEditTime: 2024-03-16 22:52:38
+ * @FilePath: /k-infra/doyin_sdk/doc.go
+ * @description: type some description
+ */
+/*
+about douyin platform feature
+*/
+package doyinsdk
