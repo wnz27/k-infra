2a96d2c4ea0286e3fb6d352e7b165af1b02f83b3
wnz27 <wnz27@icloud.com>
2024-03-16 22:29:23 +0800

    feat: update module name

--- douyin/main.go
+++ douyin/main.go
@@ -2,14 +2,15 @@
  * @Author: 27
  * @LastEditors: 27
  * @Date: 2024-03-16 18:33:55
- * @LastEditTime: 2024-03-16 18:34:00
- * @FilePath: /k-go-infra/douyin/main.go
+ * @LastEditTime: 2024-03-16 22:23:58
+ * @FilePath: /k-infra/douyin/main.go
  * @description: type some description
  */
 package douyin
 
 import "fmt"
 
+// test
 func DoyinTestDemo() {
 	fmt.Println("This is douyin test demo")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module k-infra.dev
+module github.com/wnz27/k-infra
 
 go 1.19
