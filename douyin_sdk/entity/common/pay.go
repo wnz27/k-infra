9a940ee3e5ea4c1cfb4ad25d2a40eaf6807d83bc
wnz27 <wnz27@icloud.com>
2024-03-20 12:10:52 +0800

    feat: fix base model

--- douyin_sdk/entity/common/pay.go
+++ douyin_sdk/entity/common/pay.go
@@ -2,7 +2,7 @@
  * @Author: 27
  * @LastEditors: 27
  * @Date: 2024-03-19 23:38:05
- * @LastEditTime: 2024-03-20 12:03:01
+ * @LastEditTime: 2024-03-20 12:08:14
  * @FilePath: /k-infra/douyin_sdk/entity/common/pay.go
  * @description: type some description
  */
@@ -60,7 +60,6 @@ type PayCallBackReqAllData struct {
 }
 
 type BaseDouyinOrderInfo struct {
-	OutOrderNo     string             `json:"out_order_no"`     //  开发者系统生成的订单号，与抖音开平交易单号 order_id 唯一关联，长度 <= 64byte
 	OrderID        string             `json:"order_id"`         //  抖音开平侧订单id，长度 <= 64byte
 	Status         CallBackPayStatus  `json:"status"`           //  支付结果状态，目前有两种状态： "SUCCESS" （支付成功 ） "CANCEL" （支付取消）
 	TotalAmount    int64              `json:"total_amount"`     //  订单总金额，单位分支付金额为 = total_amount - discount_amount
@@ -76,7 +75,12 @@ type BaseDouyinOrderInfo struct {
 // 由 PayCallBackRequest 的 msg Unmarshal 而来
 // From PayCallBackRequest.Msg Unmarshal
 type PayCallBackInfo struct {
-	AppID string `json:"app_id"`
+	// 跟开发者相关的信息
+	// developer info
+	AppID      string `json:"app_id"`
+	OutOrderNo string `json:"out_order_no"` //  开发者系统生成的订单号，与抖音开平交易单号 order_id 唯一关联，长度 <= 64byte
+	// 抖音开发平台内部的信息
+	// douyin platform info
 	BaseDouyinOrderInfo
 }
 
