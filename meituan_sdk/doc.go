049c61b79023c33f679f2709c5b494b4cb0066a8
wnz27 <wnz27@icloud.com>
2026-02-09 16:16:14 +0800

    feat: init meituan

---
+++ meituan_sdk/doc.go
@@ -0,0 +1,13 @@
+/*
+ * @Author: 27
+ * @LastEditors: 27
+ * @Date: 2026-02-09 15:26:59
+ * @LastEditTime: 2026-02-09 15:27:10
+ * @FilePath: /k-infra/meituan_sdk/doc.go
+ * @description: type some description
+ */
+/*
+about meituan platform feature
+美团开发平台相关功能接入
+*/
+package meituan_sdk
